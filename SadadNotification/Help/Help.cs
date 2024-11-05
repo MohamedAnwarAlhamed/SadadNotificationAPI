b68f08ca8d80cfaf9ce34e1e4aecd8a447b500d4
Mohammed Anwar <Alhamed202024@gmail.com>
2024-11-05 22:30:06 +0300

    Add project files.

---
+++ SadadNotification/Help/Help.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+
+namespace SadadNotification.Help
+{
+    public static class Helping
+    {
+        public static bool VerifySignature(byte[] data, byte[] signature, RSA publicKey)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                // حساب هاش البيانات الأصلية
+                byte[] hash = sha256.ComputeHash(data);
+
+                // التحقق من التوقيع
+                return publicKey.VerifyHash(hash, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+        }
+    }
+}
---
+++ SadadNotification/Model/NotificationData.cs
@@ -0,0 +1,18 @@
+﻿namespace SadadNotification.Model
+{
+    public class NotificationData
+    {
+        public string branchCode { get; set; }
+        public string bankId { get; set; }
+        public string districtCode { get; set; }
+        public string accessChannel { get; set; }
+        public string sadadPaymentId { get; set; }
+        public string sadadNumber { get; set; }
+        public string paymentMethod { get; set; }
+        public string billNumber { get; set; }
+        public string bankPaymentId { get; set; }
+        public string paymentDate { get; set; }
+        public decimal paymentAmount { get; set; }
+        public string paymentStatus { get; set; }
+    }
+}
