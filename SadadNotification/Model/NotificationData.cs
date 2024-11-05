14d97e28a509c0080c0cf0b239b5640b4bbae704
Mohammed Anwar <Alhamed202024@gmail.com>
2024-11-05 22:48:18 +0300

    fix a problem for deleting a file NotificationController

--- SadadNotification/Model/NotificationData.cs
+++ SadadNotification/Model/NotificationData.cs
@@ -11,7 +11,7 @@
         public string paymentMethod { get; set; }
         public string billNumber { get; set; }
         public string bankPaymentId { get; set; }
-        public string paymentDate { get; set; }
+        public DateTime paymentDate { get; set; }
         public decimal paymentAmount { get; set; }
         public string paymentStatus { get; set; }
     }
