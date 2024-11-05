012ac1a4e1650d1c3a98f8ca6f7144bfcd2fb42b
Mohammed Anwar <Alhamed202024@gmail.com>
2024-11-06 00:04:35 +0300

    adding the database and insert Operations

--- SadadNotification/Controllers/NotificationsController.cs
+++ SadadNotification/Controllers/NotificationsController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using SadadNotification.Help;
 using SadadNotification.Model;
+using System.Data.SqlClient;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Xml;
@@ -12,14 +15,19 @@ namespace SadadNotification.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class NotificationsController : ControllerBase 
+    public class NotificationsController : ControllerBase
     {
-        
-
-        [HttpPost("PaymentNotification")]
-        public IActionResult ReceivePaymentNotification([FromBody] NotificationData request)
+        private readonly IConfiguration _configuration;
+        private DatabaseOperations _databaseOperations;
+        public NotificationsController(IConfiguration configuration)
         {
-           
+            _configuration = configuration;
+            _databaseOperations = new DatabaseOperations(_configuration);
+        }
+        [HttpPost("PaymentNotification")]
+        public async Task<IActionResult> ReceivePaymentNotificationAsync([FromBody] NotificationData request)
+        {
+
             // قراءة التوقيع من رأس الطلب
             if (!Request.Headers.TryGetValue("signature", out var signature))
             {
@@ -38,22 +46,50 @@ namespace SadadNotification.Controllers
             byte[] originalData = Encoding.UTF8.GetBytes(originalJson);
 
             bool isValid = Helping.VerifySignature(originalData, Convert.FromBase64String(signature), publicKey);
-           
-            if(!isValid)
+
+            if (!isValid)
             {
+
+
                 return Unauthorized(new { status = 401, message = "Invalid Signature." });
+
             }
             else
             {
-                // يمكن إضافة منطق معالجة الدفع هنا
-                return Ok(new { status = 200, message = "Operation Done Successfully" });
+                var sadadNotification = new
+                {
+                    Guid = Guid.NewGuid(), // توليد GUID جديد
+                    BranchCode = request.branchCode,
+                    BankId = request.bankId,
+                    DistrictCode = request.districtCode,
+                    AccessChannel = request.accessChannel,
+                    SadadPaymentId = request.sadadPaymentId,
+                    SadadNumber = request.sadadNumber,
+                    PaymentMethod = request.paymentMethod,
+                    BillNumber = request.billNumber,
+                    BankPaymentId = request.bankPaymentId,
+                    PaymentDate = request.paymentDate,
+                    PaymentAmount = request.paymentAmount,
+                    PaymentStatus = request.paymentStatus,
+                    NotificationType = 1
+                };
+
+                var result = await _databaseOperations.InsertSadadNotificationAsync(sadadNotification);
+                if (result)
+                {
+                    return Ok(new { status = 200, message = "Operation Done Successfully" });
+                }
+                else
+                {
+                    return BadRequest(new { status = 400, message = "Failed to insert data" });
+                }
             }
 
-            
+
         }
 
         [HttpPost("SettlementNotification")]
-        public IActionResult ReceiveSettlementNotification([FromBody] NotificationData request)
+        public async Task<IActionResult> ReceiveSettlementNotificationAsync([FromBody] NotificationData request)
         {
             // قراءة التوقيع من رأس الطلب
             if (!Request.Headers.TryGetValue("signature", out var signature))
@@ -80,10 +116,36 @@ namespace SadadNotification.Controllers
             }
             else
             {
-                // يمكن إضافة منطق معالجة الدفع هنا
-                return Ok(new { status = 200, message = "Operation Done Successfully" });
+                var sadadNotification = new
+                {
+                    Guid = Guid.NewGuid(), // توليد GUID جديد
+                    BranchCode = request.branchCode,
+                    BankId = request.bankId,
+                    DistrictCode = request.districtCode,
+                    AccessChannel = request.accessChannel,
+                    SadadPaymentId = request.sadadPaymentId,
+                    SadadNumber = request.sadadNumber,
+                    PaymentMethod = request.paymentMethod,
+                    BillNumber = request.billNumber,
+                    BankPaymentId = request.bankPaymentId,
+                    PaymentDate = request.paymentDate,
+                    PaymentAmount = request.paymentAmount,
+                    PaymentStatus = request.paymentStatus,
+                    NotificationType = 2
+                };
+
+                var result = await _databaseOperations.InsertSadadNotificationAsync(sadadNotification);
+                if (result)
+                {
+                    return Ok(new { status = 200, message = "Operation Done Successfully" });
+                }
+                else
+                {
+                    return BadRequest(new { status = 400, message = "Failed to insert data" });
+                }
             }
 
         }
     }
 }
+    
--- SadadNotification/Help/Help.cs
+++ SadadNotification/Help/Help.cs
@@ -1,9 +1,14 @@
-﻿using System.Security.Cryptography;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
 
 namespace SadadNotification.Help
 {
     public static class Helping
     {
+        
+
         public static bool VerifySignature(byte[] data, byte[] signature, RSA publicKey)
         {
             using (var sha256 = SHA256.Create())
@@ -13,6 +18,34 @@ namespace SadadNotification.Help
 
                 // التحقق من التوقيع
                 return publicKey.VerifyHash(hash, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+        }
+        
+    }
+
+    public class DatabaseOperations
+    {
+        private readonly IConfiguration _configuration;
+        public DatabaseOperations(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<bool> InsertSadadNotificationAsync(dynamic sadadNotification)
+        {
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                await connection.OpenAsync();
+
+                var query = @"
+                INSERT INTO SadadNotificationInfo (Guid, BranchCode, BankId, DistrictCode, AccessChannel, SadadPaymentId, 
+                                                    SadadNumber, PaymentMethod, BillNumber, BankPaymentId, 
+                                                    PaymentDate, PaymentAmount, PaymentStatus, NotificationType)
+                VALUES (@Guid, @BranchCode, @BankId, @DistrictCode, @AccessChannel, @SadadPaymentId, 
+                        @SadadNumber, @PaymentMethod, @BillNumber, @BankPaymentId, 
+                        @PaymentDate, @PaymentAmount, @PaymentStatus, @NotificationType)";
+
+                var result = await connection.ExecuteAsync(query, (object)sadadNotification);
+                return result > 0; // إرجاع true إذا تم الإدراج بنجاح
             }
         }
     }
